e267457bcee0e6eb8d1ba9dcf32ecdf0bdc0c70a
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-04-15 09:54:54 +0300

    refactoring: code style

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,20 +1,20 @@
-import axios from 'axios';
+import axios from 'axios'
 
 type ApiResponseType = {
-  id: string;
-  value: string;
-  categories: [];
-  created_at: string;
-  icon_url: string;
-  updated_at: string;
-  url: string;
-};
-export type JokeType = Pick<ApiResponseType, 'value' | 'id'>;
+  id: string
+  value: string
+  categories: []
+  created_at: string
+  icon_url: string
+  updated_at: string
+  url: string
+}
+export type JokeType = Pick<ApiResponseType, 'value' | 'id'>
 
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-});
+})
 
 export const appRequest = {
   getJoke: () => instance.get<JokeType>('/'),
-};
+}
